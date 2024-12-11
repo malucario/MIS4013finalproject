7e2c77b7bac62887c1880f646b3b1ab76eb20038
malucario <122913162+malucario@users.noreply.github.com>
2024-12-11 17:18:45 -0600

    Update heisman.php

--- heisman.php
+++ heisman.php
@@ -3,7 +3,7 @@ Include "view-header.php"
 ?>
 
 <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="https://www.denverpost.com/wp-content/uploads/2024/10/TDP-Z-CU.jpg?w=525" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Travis Hunter</h5>
     <p class="card-text">Hunter has redefined what it means to be a two-way player in modern college football. Playing both wide receiver and cornerback at an elite level, Hunter has showcased rare versatility and endurance. On offense, he has racked up over 1,000 receiving yards and 12 touchdowns, proving himself a deep-threat nightmare for defenses. On the defensive side, Hunter has locked down opposing receivers with 4 interceptions and 15 pass breakups, consistently shutting down top targets. His impact on both sides of the ball is unparalleled this season, and his ability to influence the game in multiple facets makes him a once-in-a-generation talent deserving of Heisman recognition.</p>
@@ -20,7 +20,7 @@ Include "view-header.php"
 </div>
 
 <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="https://external-preview.redd.it/ashton-jeanty-sets-boise-state-single-season-rushing-record-v0-roW5KbEXoyd9PVFtLhsgjJaLfJ5m3gRcl-Kif7GQIb4.jpg?width=640&crop=smart&auto=webp&s=54f8082b3d11eb5cd8dc2cf6ccc46621b186a9c5" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Ashton Jeanty</h5>
     <p class="card-text">Jeanty has captivated the college football world with one of the greatest rushing seasons in history. Jeanty has already surpassed Jay Ajayi's 2014 Boise State record for single-season rushing yards and is on pace to break Barry Sanders' legendary NCAA record of 2,628 rushing yards set in 1988. Currently, Jeanty has amassed over 2,400 rushing yards and 28 touchdowns, averaging an incredible 8.3 yards per carry. His explosiveness has powered Boise State to an unprecedented level, securing a first-round bye in the playoffs. Jeanty’s dominance on the ground and ability to carry his team in high-pressure situations make him a strong Heisman contender.</p>
@@ -37,7 +37,7 @@ Include "view-header.php"
 </div>
 
 <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="https://www.registerguard.com/gcdn/authoring/authoring-images/2024/09/14/NREG/75227422007-bb-3-i-9691.jpg?width=1733&height=1156&fit=crop&format=pjpg&auto=webp" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Dillon Gabriel</h5>
     <p class="card-text">Gabriel has thrived under the spotlight, leading his team to a #1 national ranking in their first season in the Big Ten. Gabriel earned Big Ten Offensive Player of the Year honors by throwing for over 4,200 yards and 38 touchdowns while adding another 500 yards and 6 scores on the ground. His pinpoint accuracy (70% completion rate) and poise under pressure have been instrumental in dismantling elite defenses and securing the top seed. Gabriel’s leadership, efficiency, and role in a seamless conference transition cement his case as the best player in college football this season.</p>
@@ -54,7 +54,7 @@ Include "view-header.php"
 </div>
 
 <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="https://res.cloudinary.com/graham-media-group/image/upload/f_auto/q_auto/c_scale,w_640/v1/media/gmg/HME7TWLPWNHG3LH726RPY3W5JM.jpg?_a=DAJAUVWIZAAA" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Cam Ward</h5>
     <p class="card-text">Ward has orchestrated one of the most potent offenses in college football, showcasing a relentless ability to pick apart defenses. The dynamic signal-caller has thrown for more than 4,500 yards and 40 touchdowns while rushing for an additional 8 scores. Ward’s ability to extend plays and deliver in clutch moments has made his team one of the most dangerous in the country, averaging over 45 points per game. His creativity, decision-making, and high-powered production have set him apart as an electrifying playmaker and a legitimate Heisman contender.</p>
