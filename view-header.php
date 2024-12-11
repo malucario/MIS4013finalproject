fb048754f3f1450f5a5cf073470a48e4f1a5bde3
malucario <122913162+malucario@users.noreply.github.com>
2024-12-11 17:00:18 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -6,7 +6,7 @@
     <title>College Football Central</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
-  <body style="color: beige;">
+  <body style="background-color: beige;">
     <div class="container">
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
