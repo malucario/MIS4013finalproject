2544ce48596ef931617cd43ba580b815fee25e78
malucario <122913162+malucario@users.noreply.github.com>
2024-12-11 12:51:55 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ Include "view-header.php";
   <p> - The new 12-team College Football Playoff</p>
   <p> - The four Heisman finalists announced earlier this week.</p>
   <p> - The 125th edition of the historic Army-Navy rivalry.</p>
-  <p></p>
+  <p> </p>
   <p>Use our navbar at the top to navigate and view our previews & predictions.</p>
-  <p></p>
-  <p></p>
+  <p> </p>
+  <p> </p>
   <p>*Works are written with the help of ChatGPT for the sake of time.</p>
  <?php
 Include "view-footer.php";
