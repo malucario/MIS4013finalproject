85863f86b7e4eabd04f13bbd9975adf47bf7e54a
malucario <122913162+malucario@users.noreply.github.com>
2024-12-11 13:26:44 -0600

    Create navy-uni-video.php

---
+++ navy-uni-video.php
@@ -0,0 +1,8 @@
+<?php
+Include "view-header.php";
+?>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/nXE6noYpiVg?si=qaQoL_Lp-RvB6aid" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<?php
+Include "view-footer.php";
+?>
