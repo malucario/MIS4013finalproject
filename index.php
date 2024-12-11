c4bb379a81a6a4da7251f358fb77d427a6d6c036
malucario <122913162+malucario@users.noreply.github.com>
2024-12-11 12:48:18 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
+<?php
+Include "view-header.php";
+?>
 
+<h1>Welcome to College Football Central!</h1>
+  <p>CFB Central is the best source to catch up on what's happening in college football. This week, we feature works about:</p>
+  <p> - The new 12-team College Football Playoff</p>
+  <p> - The four Heisman finalists announced earlier this week.</p>
+  <p> - The 125th edition of the historic Army-Navy rivalry.</p>
+  <p></p>
+  <p>Use our navbar at the top to navigate and view our previews & predictions.</p>
+  <p></p>
+  <p></p>
+  <p>*Works are written with the help of ChatGPT for the sake of time.</p>
+ <?php
+Include "view-footer.php";
+?>
