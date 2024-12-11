f9548603b9f553d0698c59bb19097d5abebe0e82
malucario <122913162+malucario@users.noreply.github.com>
2024-12-11 13:11:15 -0600

    Create army-navy.php

---
+++ army-navy.php
@@ -0,0 +1,23 @@
+<?php
+Include "view-header.php"
+?>
+
+<h1>The 125th Army-Navy Game will feature something the rivalry has never seen before - a conference champion.</h1>
+<p>The annual Army-Navy game is one of the most storied rivalries in college football, steeped in tradition and intensity. This year’s matchup promises to deliver another classic battle, but all signs point to Army riding high after an exceptional season and poised to take home a convincing victory.</p>
+<h3>Momentum on Army’s Side</h3>
+<p>Army enters the game fresh off a historic triumph, securing their first-ever conference championship with a dominant win in the Commander’s Classic last week. This victory not only solidifies their position as a rising force but also injects the team with unparalleled confidence heading into their biggest rivalry of the year. The Black Knights have shown they can rise to the occasion, and their momentum gives them a significant edge.</p>
+<h3>Bryson Daily Leading the Charge</h3>
+<p>At the heart of Army’s success is quarterback Bryson Daily, who has emerged as a true game-changer. Daily’s dual-threat capabilities have kept defenses on their heels all season. He has showcased an uncanny ability to extend plays with his legs, deliver clutch throws, and maintain composure under pressure. Against Navy’s stout defense, Daily’s dynamic playmaking will be the key to breaking the game open.</p>
+<h3>The Matchup: Army's Versatility vs. Navy's Consistency</h3>
+<p>Navy, as always, brings a disciplined and relentless approach. Their triple-option offense is methodical, but Army’s defense, anchored by a disciplined front seven, has proven capable of containing even the most complex rushing attacks. On offense, Army’s improved balance and consistency, with Daily leading a versatile attack, contrasts sharply with Navy’s boom-or-bust offense. This adaptability gives Army a clear path to controlling the game and capitalizing on opportunities.</p>
+<h3>Prediction</h3>
+<p>Despite the high of winning a conference championship last week and the announcement of a bowl game to be played later this month, Army's laser focus on Navy hasn't changed. I predict Army to win and cover and for the battle at the line of scrimmage to be the key point of emphasis for a win.</p>
+
+<h1> </h1>
+<p>In addition to the product on the field, this rivalry also always produces an awesome uniform battle.</p>
+<p><a href="army-uni-video.php">Click here to learn the story of Army's 101st Airborn Division-inspired uniforms.</a></p>
+<p><a href="navy-uni-video.php">Click here to learn the story of Navy's Jolly Roger uniforms.</a></p>
+
+<?php
+Include "view-footer.php"
+?>
