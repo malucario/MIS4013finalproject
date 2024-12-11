b328705ced18fbbafd302daa729d37169b2272a4
malucario <122913162+malucario@users.noreply.github.com>
2024-12-11 17:15:36 -0600

    Update heisman.php

--- heisman.php
+++ heisman.php
@@ -6,16 +6,16 @@ Include "view-header.php"
   <img src="..." class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Travis Hunter</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p class="card-text">Hunter has redefined what it means to be a two-way player in modern college football. Playing both wide receiver and cornerback at an elite level, Hunter has showcased rare versatility and endurance. On offense, he has racked up over 1,000 receiving yards and 12 touchdowns, proving himself a deep-threat nightmare for defenses. On the defensive side, Hunter has locked down opposing receivers with 4 interceptions and 15 pass breakups, consistently shutting down top targets. His impact on both sides of the ball is unparalleled this season, and his ability to influence the game in multiple facets makes him a once-in-a-generation talent deserving of Heisman recognition.</p>
   </div>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item">An item</li>
-    <li class="list-group-item">A second item</li>
-    <li class="list-group-item">A third item</li>
+    <li class="list-group-item">HT/WT: 6'1", 185 lbs</li>
+    <li class="list-group-item">Position: WR/CB</li>
+    <li class="list-group-item">School: Colorado</li>
   </ul>
   <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
+    <a href="hunter-stats.php" class="card-link">View stats</a>
+    <a href="hunter-highlights.php" class="card-link">View highlights</a>
   </div>
 </div>
 
@@ -23,16 +23,16 @@ Include "view-header.php"
   <img src="..." class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Ashton Jeanty</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p class="card-text">Jeanty has captivated the college football world with one of the greatest rushing seasons in history. Jeanty has already surpassed Jay Ajayi's 2014 Boise State record for single-season rushing yards and is on pace to break Barry Sanders' legendary NCAA record of 2,628 rushing yards set in 1988. Currently, Jeanty has amassed over 2,400 rushing yards and 28 touchdowns, averaging an incredible 8.3 yards per carry. His explosiveness has powered Boise State to an unprecedented level, securing a first-round bye in the playoffs. Jeanty’s dominance on the ground and ability to carry his team in high-pressure situations make him a strong Heisman contender.</p>
   </div>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item">An item</li>
-    <li class="list-group-item">A second item</li>
-    <li class="list-group-item">A third item</li>
+    <li class="list-group-item">HT/WT: 5'9" 215 lbs</li>
+    <li class="list-group-item">Position: RB</li>
+    <li class="list-group-item">School: Boise State</li>
   </ul>
   <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
+    <a href="jeanty-stats.php" class="card-link">View stats</a>
+    <a href="jeanty-highlights.php" class="card-link">View highlights</a>
   </div>
 </div>
 
@@ -40,16 +40,16 @@ Include "view-header.php"
   <img src="..." class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Dillon Gabriel</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p class="card-text">Gabriel has thrived under the spotlight, leading his team to a #1 national ranking in their first season in the Big Ten. Gabriel earned Big Ten Offensive Player of the Year honors by throwing for over 4,200 yards and 38 touchdowns while adding another 500 yards and 6 scores on the ground. His pinpoint accuracy (70% completion rate) and poise under pressure have been instrumental in dismantling elite defenses and securing the top seed. Gabriel’s leadership, efficiency, and role in a seamless conference transition cement his case as the best player in college football this season.</p>
   </div>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item">An item</li>
-    <li class="list-group-item">A second item</li>
-    <li class="list-group-item">A third item</li>
+    <li class="list-group-item">HT/WT: 6'0", 200 lbs</li>
+    <li class="list-group-item">Position: QB</li>
+    <li class="list-group-item">School: Oregon</li>
   </ul>
   <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
+    <a href="gabriel-stats.php" class="card-link">View stats</a>
+    <a href="gabriel-highlights.php" class="card-link">View highlights</a>
   </div>
 </div>
 
@@ -57,16 +57,16 @@ Include "view-header.php"
   <img src="..." class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Cam Ward</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p class="card-text">Ward has orchestrated one of the most potent offenses in college football, showcasing a relentless ability to pick apart defenses. The dynamic signal-caller has thrown for more than 4,500 yards and 40 touchdowns while rushing for an additional 8 scores. Ward’s ability to extend plays and deliver in clutch moments has made his team one of the most dangerous in the country, averaging over 45 points per game. His creativity, decision-making, and high-powered production have set him apart as an electrifying playmaker and a legitimate Heisman contender.</p>
   </div>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item">An item</li>
-    <li class="list-group-item">A second item</li>
-    <li class="list-group-item">A third item</li>
+    <li class="list-group-item">HT/WT: 6'2", 223 lbs</li>
+    <li class="list-group-item">Position: QB</li>
+    <li class="list-group-item">School: Miami (FL)</li>
   </ul>
   <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
+    <a href="ward-stats.php" class="card-link">View stats</a>
+    <a href="ward-highlights.php" class="card-link">View highlights</a>
   </div>
 </div>
 
