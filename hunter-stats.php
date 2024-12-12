475ddee651805bacd87df804cbc3df91200f7c57
malucario <122913162+malucario@users.noreply.github.com>
2024-12-12 12:21:04 -0600

    Update hunter-stats.php

--- hunter-stats.php
+++ hunter-stats.php
@@ -9,21 +9,21 @@ Include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertStats($_POST['hsDate'], $_POST['hsOpp'], $_POST['hsRushAtt'], $_POST['hsRushYds'], $_POST['hsRushAvg'], $_POST['hsRushTDs'], $_POST['hsRec'], $_POST['hsRecYds'], $_POST['hsRecAvg'], $_POST['hsRecTDs'], $_POST['hsDefTack'], $_POST['hsDefInt'], $_POST['hsDefPBU'])) {
+      if (insertHunterStats($_POST['hsDate'], $_POST['hsOpp'], $_POST['hsRushAtt'], $_POST['hsRushYds'], $_POST['hsRushAvg'], $_POST['hsRushTDs'], $_POST['hsRec'], $_POST['hsRecYds'], $_POST['hsRecAvg'], $_POST['hsRecTDs'], $_POST['hsDefTack'], $_POST['hsDefInt'], $_POST['hsDefPBU'])) {
         echo '<div class="alert alert-success" role="alert">Stats added.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
       break;
     case "Delete":
-      if (deleteStats($_POST['hsGLID'])) {
+      if (deleteHunterStats($_POST['hsGLID'])) {
         echo '<div class="alert alert-success" role="alert">Stats deleted.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
       break;
     case "Edit":
-      if (updateStats($_POST['hsRushAtt'], $_POST['hsRushYds'], $_POST['hsRushAvg'], $_POST['hsRushTDs'], $_POST['hsRec'], $_POST['hsRecYds'], $_POST['hsRecAvg'], $_POST['hsRecTDs'], $_POST['hsDefTack'], $_POST['hsDefInt'], $_POST['hsDefPBU'], $_POST['hsGLID'])) {
+      if (updateHunterStats($_POST['hsRushAtt'], $_POST['hsRushYds'], $_POST['hsRushAvg'], $_POST['hsRushTDs'], $_POST['hsRec'], $_POST['hsRecYds'], $_POST['hsRecAvg'], $_POST['hsRecTDs'], $_POST['hsDefTack'], $_POST['hsDefInt'], $_POST['hsDefPBU'], $_POST['hsGLID'])) {
         echo '<div class="alert alert-success" role="alert">Stats edited.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
