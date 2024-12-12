758f9b1680941f573d2c73ef5081e2f05c63a35c
malucario <122913162+malucario@users.noreply.github.com>
2024-12-12 16:11:26 -0600

    Update view-hunter-stats-editform.php

--- view-hunter-stats-editform.php
+++ view-hunter-stats-editform.php
@@ -11,7 +11,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="editJeantyStatsModalLabel<?php echo $hgamelog['GLID'];?>">Edit stats</h1>
+        <h1 class="modal-title fs-5" id="editHunterStatsModalLabel<?php echo $hgamelog['GLID'];?>">Edit stats</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
