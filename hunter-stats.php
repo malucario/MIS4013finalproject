8a784786131b92c9003c986c17b6adabe2912477
malucario <122913162+malucario@users.noreply.github.com>
2024-12-12 11:22:48 -0600

    Update hunter-stats.php

--- hunter-stats.php
+++ hunter-stats.php
@@ -9,21 +9,21 @@ Include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertStats($_POST['stID'], $_POST['stPID'], $_POST['stPA'], $_POST['stAB'], $_POST['stOPS'], $_POST['stEV'])) {
+      if (insertStats($_POST['hsDate'], $_POST['hsOpp'], $_POST['hsRushAtt'], $_POST['hsRushYds'], $_POST['hsRushAvg'], $_POST['hsRushTDs'], $_POST['hsRec'], $_POST['hsRecYds'], $_POST['hsRecAvg'], $_POST['hsRecTDs'], $_POST['hsDefTack'], $_POST['hsDefInt'], $_POST['hsDefPBU'])) {
         echo '<div class="alert alert-success" role="alert">Stats added.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
       break;
     case "Delete":
-      if (deleteStats($_POST['stID'])) {
+      if (deleteStats($_POST['hsGLID'])) {
         echo '<div class="alert alert-success" role="alert">Stats deleted.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
       break;
     case "Edit":
-      if (updateStats($_POST['stPA'], $_POST['stAB'], $_POST['stOPS'], $_POST['stEV'], $_POST['stID'])) {
+      if (updateStats($_POST['hsRushAtt'], $_POST['hsRushYds'], $_POST['hsRushAvg'], $_POST['hsRushTDs'], $_POST['hsRec'], $_POST['hsRecYds'], $_POST['hsRecAvg'], $_POST['hsRecTDs'], $_POST['hsDefTack'], $_POST['hsDefInt'], $_POST['hsDefPBU'], $_POST['hsGLID'])) {
         echo '<div class="alert alert-success" role="alert">Stats edited.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
