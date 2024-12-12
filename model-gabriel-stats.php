9d74fa0a592efc530be8b0ce01c500658f1bbff8
malucario <122913162+malucario@users.noreply.github.com>
2024-12-12 17:42:19 -0600

    Update model-gabriel-stats.php

--- model-gabriel-stats.php
+++ model-gabriel-stats.php
@@ -13,11 +13,11 @@ function selectGabrielStats() {
     }
 }
 
-function insertGabrielStats($gsDate, $gsOpp, $gsPassComps, $gsPassAtt, $gsCompPct, $gsPassYds, $gsPassTDs, $gsPassInt, $gsPassRate, $gsRushAtt, $gsRushYds, $gsRushAvg, $gsRushTDs) {
+function insertGabrielStats($gsDate, $gsOpp, $gsPassComps, $gsPassAtt, $gsCompPct, $gsPassYds, $gsPassTDs, $gsPassInts, $gsPassRate, $gsRushAtt, $gsRushYds, $gsRushAvg, $gsRushTDs) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("INSERT INTO GameLog (GLHeismanID, GLDate, GLOpponent, GLPassComps, GLPassAtt, GLCompPct, GLPassYds, GLPassTDs, GLPassInts, GLPassRate, GLRushAtt, GLRushYds, GLRushAvg, GLRushTDs, ) VALUES (3,?,?,?,?,?,?,?,?,?,?,?,?,?)");
-        $stmt->bind_param("ssiiiiiiiiiii", $gsDate, $gsOpp, $gsPassComps, $gsPassAtt, $gsCompPct, $gsPassYds, $gsPassTDs, $gsPassInt, $gsPassRate, $gsRushAtt, $gsRushYds, $gsRushAvg, $gsRushTDs); 
+        $stmt->bind_param("ssiiiiiiiiiii", $gsDate, $gsOpp, $gsPassComps, $gsPassAtt, $gsCompPct, $gsPassYds, $gsPassTDs, $gsPassInts, $gsPassRate, $gsRushAtt, $gsRushYds, $gsRushAvg, $gsRushTDs); 
         $success = $stmt->execute();
         $conn->close();
         return $success;
@@ -27,11 +27,11 @@ function insertGabrielStats($gsDate, $gsOpp, $gsPassComps, $gsPassAtt, $gsCompPc
     }
 }
 
-function updateGabrielStats($gsPassComps, $gsPassAtt, $gsCompPct, $gsPassYds, $gsPassTDs, $gsPassInt, $gsPassRate, $gsRushAtt, $gsRushYds, $gsRushAvg, $gsRushTDs, $gsGLID) {
+function updateGabrielStats($gsPassComps, $gsPassAtt, $gsCompPct, $gsPassYds, $gsPassTDs, $gsPassInts, $gsPassRate, $gsRushAtt, $gsRushYds, $gsRushAvg, $gsRushTDs, $gsGLID) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("UPDATE GameLog SET GLPassComps=?, GLPassAtt=?, GLCompPct=?, GLCompPct, GLPassTDs=?, GLPassInts=?, GLPassRate=?, GLRushAtt=?, GLRushYds=?, GLRushAvg=?, GLRushTDs=? WHERE GLID=?");
-        $stmt->bind_param("iiiiiiiiiiii", $gsPassComps, $gsPassAtt, $gsPassYds, $gsCompPct, $gsPassTDs, $gsPassInt, $gsPassRate, $gsRushAtt, $gsRushYds, $gsRushAvg, $gsRushTDs, $gsGLID); 
+        $stmt->bind_param("iiiiiiiiiiii", $gsPassComps, $gsPassAtt, $gsPassYds, $gsCompPct, $gsPassTDs, $gsPassInts, $gsPassRate, $gsRushAtt, $gsRushYds, $gsRushAvg, $gsRushTDs, $gsGLID); 
         $success = $stmt->execute();
         $conn->close();
         return $success;
@@ -58,7 +58,7 @@ function deleteGabrielStats($gsGLID) {
 function selectGabrielTotalStats() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT SUM(GLPassComps) AS TotalGPassComps, SUM(GLPassAtt) AS TotalGPassAtt, ROUND((SUM(GLPassComps)/SUM(GLPassAtt))*100) AS TotalGCompPct, SUM(GLPassYds) AS TotalGPassYds, SUM(GLPassTDs) AS TotalGPassTDs, SUM(GLPassInts) AS TotalGPassInt, AVG(GLPassRate) AS TotalGPassRate, SUM(GLRushAtt) AS TotalGRushAtt, SUM(GLRushYds) AS TotalGRushYds, ROUND(SUM(GLRushYds)/SUM(GLRushAtt),1) AS TotalGRushAvg, SUM(GLRushTDs) AS TotalGRushTDs FROM GameLog WHERE GLHeismanID=3");
+        $stmt = $conn->prepare("SELECT SUM(GLPassComps) AS TotalGPassComps, SUM(GLPassAtt) AS TotalGPassAtt, ROUND((SUM(GLPassComps)/SUM(GLPassAtt))*100) AS TotalGCompPct, SUM(GLPassYds) AS TotalGPassYds, SUM(GLPassTDs) AS TotalGPassTDs, SUM(GLPassInts) AS TotalGPassInts, AVG(GLPassRate) AS TotalGPassRate, SUM(GLRushAtt) AS TotalGRushAtt, SUM(GLRushYds) AS TotalGRushYds, ROUND(SUM(GLRushYds)/SUM(GLRushAtt),1) AS TotalGRushAvg, SUM(GLRushTDs) AS TotalGRushTDs FROM GameLog WHERE GLHeismanID=3");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
