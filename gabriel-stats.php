24870b9f762eb6913818d3cd233bb051263bc168
malucario <122913162+malucario@users.noreply.github.com>
2024-12-12 17:36:11 -0600

    Update gabriel-stats.php

--- gabriel-stats.php
+++ gabriel-stats.php
@@ -9,7 +9,7 @@ Include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertGabrielStats($_POST['gsDate'], $_POST['gsOpp'], $_POST['gsPassComps'], $_POST['gsPassAtt'], $_POST['gsCompPct'], $_POST['gsPassTDs'], $_POST['gsPassInt'], $_POST['gsPassRate'], $_POST['gsRushAtt'], $_POST['gsRushYds'], $_POST['gsRushAvg'], $_POST['gsRushTDs'])) {
+      if (insertGabrielStats($_POST['gsDate'], $_POST['gsOpp'], $_POST['gsPassComps'], $_POST['gsPassAtt'], $_POST['gsCompPct'], $_POST['gsPassYds'], $_POST['gsPassTDs'], $_POST['gsPassInt'], $_POST['gsPassRate'], $_POST['gsRushAtt'], $_POST['gsRushYds'], $_POST['gsRushAvg'], $_POST['gsRushTDs'])) {
         echo '<div class="alert alert-success" role="alert">Stats added.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
@@ -23,7 +23,7 @@ if (isset($_POST['actionType'])) {
       }
       break;
     case "Edit":
-      if (updateGabrielStats($_POST['gsPassComps'], $_POST['gsPassAtt'], $_POST['gsCompPct'], $_POST['gsPassTDs'], $_POST['gsPassInt'], $_POST['gsPassRate'], $_POST['gsRushAtt'], $_POST['gsRushYds'], $_POST['gsRushAvg'], $_POST['gsRushTDs'], $_POST['gsGLID'])) {
+      if (updateGabrielStats($_POST['gsPassComps'], $_POST['gsPassAtt'], $_POST['gsCompPct'], $_POST['gsPassYds'], $_POST['gsPassTDs'], $_POST['gsPassInt'], $_POST['gsPassRate'], $_POST['gsRushAtt'], $_POST['gsRushYds'], $_POST['gsRushAvg'], $_POST['gsRushTDs'], $_POST['gsGLID'])) {
         echo '<div class="alert alert-success" role="alert">Stats edited.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
@@ -33,8 +33,8 @@ if (isset($_POST['actionType'])) {
 }
 
 
-$jgamelogs = selectGabrielStats();
-$jtotals = selectGabrielTotalStats();
+$ggamelogs = selectGabrielStats();
+$gtotals = selectGabrielTotalStats();
 include "view-gabriel-stats.php";
 Include "view-footer.php";
 ?>
