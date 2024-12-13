5321a9b7aab82bf930c616fd0bdd57381349124f
malucario <122913162+malucario@users.noreply.github.com>
2024-12-12 22:31:58 -0600

    Update cfp.php

--- cfp.php
+++ cfp.php
@@ -2,13 +2,25 @@
 Include "view-header.php";
 ?>
 
-<h1>Welcome to the New 12-team Expanded Playoff</h1>
+<h1>Welcome to the New 12-team Expanded Playoff Era</h1>
 <sh1>Whether you like it or not, it's here. So we're here to break it down for you.</sh1>
 
-<?php
-Include "view-bracket.php";
-?>
+<h3><br>Teams and Matchups</h3>
+<p>The top four seeds are this season's four highest-ranked conference champions:</p>
+<p><strong>#1. Oregon (Big Ten Champion)</strong></p>
+<p><strong>#2. Georgia (SEC Champion)</strong></p>
+<p><strong>#3. Boise State (Mountain West Champion)</strong></p>
+<p><strong>#4. Arizona State (Big 12 Champion)</strong></p>
+<p><br>The remaining teams match up as follows in the first round:</p>
+<p><strong>#5 Texas vs. #12 Clemson (ACC Champion)</strong><br>- A clash between Texas, a dominant force in the regular season, and Clemson, fresh off their ACC title. Texas’s recently-struggling offense will be tested against Clemson’s dynamic defense.</p>
+<p><strong>#6 Penn State vs. #11 SMU</strong><br>- Penn State, boasting a strong defense, will host SMU's high-powered offense in Happy Valley. Both teams are looking to rebound from losses in their respective conference championship games.</p>
+<p><strong>#7 Notre Dame vs. #10 Indiana</strong><br>- An in-state matchup of Midwest powerhouses, as Notre Dame’s dominant run game faces an Indiana team eager to make their mark.</p>
+<p><strong>#8 Ohio State vs. #9 Tennessee</strong><br>- Ohio State’s deep roster and home-field advantage make them formidable, but Tennessee’s explosive playmakers add unpredictability to this battle.</p>
 
+<h3><br><br>Below is a visual for the playoff:</h3>
+<img src="https://www.ncaa.com/_flysystem/public-s3/styles/original/public-s3/images/2024-12-08/2024CFPBracket.png?itok=xvDi-CKJ" alt="CFP Bracket">
+
+<!--Bracket pic-->
 <?php
 Include "view-footer.php";
 ?>
