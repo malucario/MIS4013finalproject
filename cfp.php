071c22f74eda0db4cc3cbf371b02005fb4bad52b
malucario <122913162+malucario@users.noreply.github.com>
2024-12-12 21:12:44 -0600

    Create cfp.php

---
+++ cfp.php
@@ -0,0 +1,14 @@
+<?php
+Include "view-header.php";
+?>
+
+<h1>Welcome to the New 12-team Expanded Playoff</h1>
+<sh1>Whether you like it or not, it's here. So we're here to break it down for you.</sh1>
+
+<?php
+Include "view-bracket.php";
+?>
+
+<?php
+Include "view-header.php";
+?>
