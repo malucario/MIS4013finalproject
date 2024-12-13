f0f98fc11ee65e6211e29f76a788345694696ea5
malucario <122913162+malucario@users.noreply.github.com>
2024-12-12 18:38:12 -0600

    Create ward-highlights.php

---
+++ ward-highlights.php
@@ -0,0 +1,9 @@
+<?php
+Include "view-header.php"
+?>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/NFuExRksy7Q?si=0mEjGNhJULqMyoIc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+<?php
+Include "view-footer.php"
+?>
