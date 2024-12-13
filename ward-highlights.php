e259136feb5d3a90fc89182077d65c5f7bc7b2e8
malucario <122913162+malucario@users.noreply.github.com>
2024-12-12 18:42:32 -0600

    Update ward-highlights.php

--- ward-highlights.php
+++ ward-highlights.php
@@ -2,8 +2,7 @@
 Include "view-header.php";
 ?>
 
-<iframe width="560" height="315" src="https://www.youtube.com/embed/NFuExRksy7Q?si=0mEjGNhJULqMyoIc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-
+<iframe width="560" height="315" src="https://www.youtube.com/embed/T_0nPdqxjkY?si=_aVwYADsD3dPpD8X" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 <?php
 Include "view-footer.php";
 ?>
