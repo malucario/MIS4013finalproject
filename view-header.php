a08718d9119a70119a7d6d5058af645fefa6cf5d
malucario <122913162+malucario@users.noreply.github.com>
2024-12-12 21:13:55 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -9,24 +9,24 @@
   <body style="background-color: beige;">
     <div class="container">
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid" style="background-color: darkred; color: white;">
-    <a class="navbar-brand" href="#">College Football Central</a>
+  <div class="container-fluid" style="background-color: darkred;">
+    <a class="navbar-brand" href="#" style="color: white;">College Football Central</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
+          <a class="nav-link active" aria-current="page" href="/" style="color: white;">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="cfp.php">College Football Playoff</a>
+          <a class="nav-link" href="cfp.php" style="color: white;">College Football Playoff</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="heisman.php">Heisman Hopefuls</a>
+          <a class="nav-link" href="heisman.php" style="color: white;">Heisman Hopefuls</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="army-navy.php">125th Army-Navy Game</a>
+          <a class="nav-link" href="army-navy.php" style="color: white;">125th Army-Navy Game</a>
         </li>
       </ul>
     </div>
