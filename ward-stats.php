155cb6f4547401665c2bcf35a8434fd045596468
malucario <122913162+malucario@users.noreply.github.com>
2024-12-12 18:16:46 -0600

    Create ward-stats.php

---
+++ ward-stats.php
@@ -0,0 +1,40 @@
+<?php
+require_once("util-db.php");
+require_once("model-ward-stats.php");
+$pageTitle = "Cam Ward Stats";
+
+Include "view-header.php";
+
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertWardStats($_POST['wsDate'], $_POST['wsOpp'], $_POST['wsPassComps'], $_POST['wsPassAtt'], $_POST['wsCompPct'], $_POST['wsPassYds'], $_POST['wsPassTDs'], $_POST['wsPassInts'], $_POST['wsPassRate'], $_POST['wsRushAtt'], $_POST['wsRushYds'], $_POST['wsRushAvg'], $_POST['wsRushTDs'])) {
+        echo '<div class="alert alert-success" role="alert">Stats added.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+    case "Delete":
+      if (deleteWardStats($_POST['wsGLID'])) {
+        echo '<div class="alert alert-success" role="alert">Stats deleted.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+    case "Edit":
+      if (updateWardStats($_POST['wsPassComps'], $_POST['wsPassAtt'], $_POST['wsCompPct'], $_POST['wsPassYds'], $_POST['wsPassTDs'], $_POST['wsPassInts'], $_POST['wsPassRate'], $_POST['wsRushAtt'], $_POST['wsRushYds'], $_POST['wsRushAvg'], $_POST['wsRushTDs'], $_POST['wsGLID'])) {
+        echo '<div class="alert alert-success" role="alert">Stats edited.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+  }
+}
+
+
+$ggamelows = selectWardStats();
+$gtotals = selectWardTotalStats();
+include "view-ward-stats.php";
+Include "view-footer.php";
+?>
